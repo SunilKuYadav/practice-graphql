8fe549b11fb1e1946c6c6bb0a48a0383406b3b97
SunilKuYadav <sunil.yadav@moonshinetechnology.com>
2024-12-17 16:39:27 +0530

    Running Express + GraphQL

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const { createHandler } = require("graphql-http/lib/use/express");
+const { buildSchema } = require("graphql");
+const { ruruHTML } = require("ruru/server");
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -7,16 +10,31 @@ const schema = buildSchema(`
     }
 `);
 
-// The rootValue provides a resolver function for each API endpoint
-const rootValue = {
+// The root provides a resolver function for each API endpoint
+const root = {
   hello() {
     return "Hello World";
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue
-}).then(res => console.log(res))
+const app = express();
+
+// create and use GraphQL handler.
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue: root,
+  })
+);
+
+// start the server
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
+})
